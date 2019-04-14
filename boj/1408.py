25bcd923a85278aac963b2e8a2244fd062dda4d8
andole87 <andole87@gmail.com>
2019-04-14 14:02:40 +0900

    boj 1408

--- boj/1408.py
+++ boj/1408.py
@@ -1,8 +1,30 @@
-h,m,s = map(int,input().split(":"))
-hh,mm,ss = map(int,input().split(":"))
+def get_sec(a):
+    return a[0]*3600 + a[1]*60 + a[2]
+def get_time(a):
+    result = []
+    result.append(a//3600)
+    a = a % 3600
+    result.append(a//60)
+    a = a % 60
+    result.append(a)
+    return result
+
+def get_format(a):
+    result = []
+    if a[0] > 23:
+        a[0] -= 24
+    for x in a:
+        if len(str(x)) == 1:
+            result.append("0"+str(x))
+        else:
+            result.append(str(x))
+    return result
 
 
+a = list(map(int,input().split(":")))
+b = list(map(int,input().split(":")))
+rs = []
 
-
-
-
+b[0] = b[0] + 24
+x,y = get_sec(a),get_sec(b)
+print(":".join(get_format(get_time(y-x))))
