27e1f9f36ae2e63353deda00fc7322a67423ac30
andole87 <andole87@gmail.com>
2019-05-06 13:02:24 +0900

    boj 1260

---
+++ boj/1260.py
@@ -0,0 +1,40 @@
+N, M, V = map(int, input().split())
+edge = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    n1, n2 = map(int, input().split())
+    edge[n1].append(n2)
+    edge[n2].append(n1)
+
+for e in edge:
+    e.sort(reverse=True)
+
+def dfs():
+    d_visit = []
+    stack = [V]
+    d_check = [0 for _ in range(N+1)]
+    while stack:
+        v1 = stack.pop()
+        if not d_check[v1]:
+            d_check[v1] = 1
+            d_visit.append(v1)
+            stack += edge[v1]
+    return d_visit
+
+
+def bfs():
+    b_visit = []
+    queue = [V]
+    b_check = [0 for _ in range(N+1)]
+    b_check[V] = 1
+    while queue:
+        v2 = queue.pop()
+        b_visit.append(v2)
+        for i in reversed(edge[v2]):
+            if not b_check[i]:
+                b_check[i] = 1
+                queue = [i] + queue
+    return b_visit
+
+print(' '.join(map(str,dfs())))
+print(' '.join(map(str,bfs())))
