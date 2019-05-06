7a39fb0ece806e5d8a3facd4aaaedd29ae2b5e0c
andole87 <andole87@gmail.com>
2019-05-06 13:09:07 +0900

    named DFS

--- named_algorithm/DFS.py
+++ named_algorithm/DFS.py
@@ -1,20 +1,32 @@
 # 정점은 N, 간선은 V
 N, V = 0, 0
-# 인접행렬 방식
-graph_adj_list = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-# 정점에 방문했는지 여부를 기록
-visited = [0 for _ in range(N + 1)]
+# 인접리스트
+graph_adj_list = [[] for _ in range(N + 1)]
 
 # 재귀호출
-def dfs(node):
+def dfs_recursive(node, visited):
     print("DFS visits " + node + "\n")
     visited[node] = 1
     for i in range(len(graph_adj_list[node])):
         there = graph_adj_list[node][i]
         if not visited[there]:
-            dfs(there)
+            dfs_recursive(there, visited)
 
 
-for i in range(len(graph_adj_list)):
-    if not visited[i]:
-        dfs(i)
+def dfsAll():
+    visited = [0 for _ in range(N + 1)]
+    for i in range(len(graph_adj_list)):
+        if not visited[i]:
+            dfs_recursive(i, visited)
+
+
+## 스택
+def dfs_stack(start):
+    stack = [start]
+    visited = []
+    while stack:
+        next = stack.pop()
+        if not next in visited:
+            visited.append(next)
+            stack += graph_adj_list[next]
+    return visited
