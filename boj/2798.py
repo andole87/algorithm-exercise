751e845c22f030af8d27241a7fa8bc0981994c9b
andole87 <andole87@gmail.com>
2019-05-25 13:30:09 +0900

    boj 2798

--- boj/2798.py
+++ boj/2798.py
@@ -1,7 +1,13 @@
 def blackjack(a, b):
-    
-
-
-
-n,m = int(input().split())
-c = sorted(list(map(int,input().split())),reverse = True)
+    if b[0] + b[1] + b[2] <= a:
+        return b[0] + b[1] + b[2]
+    result = []
+    for i in range(len(b)):
+        for j in range(i+1,len(b)):
+            for k in range(j+1,len(b)):
+                if a >= b[i] + b[j] + b[k]:
+                    result.append(b[i] + b[j] + b[k])
+    return max(result)
+n,m = map(int,input().split())
+c = sorted(list(map(int,input().split())), reverse=True)
+print(blackjack(m,c))
