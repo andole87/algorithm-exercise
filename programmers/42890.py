f70466bf0744910d92e1299bd48132187ad720db
andole87 <andole87@gmail.com>
2019-09-07 22:35:42 +0900

    pro 42890

---
+++ programmers/42890.py
@@ -0,0 +1,18 @@
+def solution(relation):
+    return list(map(list, zip(*relation)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+relation = [["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
+print(solution(relation))
