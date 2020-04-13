dbd2636515a0029022bd228b63cda9d2b6b9f460
andole98 <andole87@gmail.com>
2020-04-13 16:07:28 +0900

    boj: 5337

---
+++ boj/java/B5337.java
@@ -0,0 +1,8 @@
+public class B5337 {
+    public static void main(String[] args) {
+        String s = ".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.";
+        System.out.println(s);
+    }
+}
