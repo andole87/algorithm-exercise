f34fc7eac3da7567a397fdc1199cf8398d33a94c
andole98 <andole87@gmail.com>
2020-04-14 16:07:28 +0900

    boj: 5338

---
+++ boj/java/B5338.java
@@ -0,0 +1,10 @@
+public class B5338 {
+    public static void main(String[] args) {
+        String s = "       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|";
+        System.out.println(s);
+    }
+}
