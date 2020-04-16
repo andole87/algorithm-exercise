b7cf13f4d51d5015d878dde82b6c4df0ec5e87b1
andole98 <andole87@gmail.com>
2020-04-16 16:07:28 +0900

    pro: pipe

--- programmers/queuestack/pipe.py
+++ programmers/queuestack/pipe.py
@@ -1,11 +1,14 @@
 arrangement = arrangement.replace("()", ' ').strip()
 answer = 0
-steel = 0
-for tag in arrangement:
-        if tag == '(':
-            steel += 1
-        elif tag == ')':
-
-        else:
-            steel = [s+1 for s in steel]
-    return answer
+arrangement = arrangement.replace("()"," ").strip()
+answer = 0
+steel = []
+for x in arrangement:
+    if x == '(':
+        steel.append('(')
+    elif x == ')':
+        steel.pop()
+        answer += 1
+    else:
+        answer += len(steel)
+return answer
