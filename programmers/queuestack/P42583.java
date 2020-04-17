74b5e990d7a6616dcdc68773cc0e9d61ea3a9d9b
andole98 <andole87@gmail.com>
2020-04-17 16:07:28 +0900

    pro: 42583

--- programmers/queuestack/P42583.java
+++ programmers/queuestack/P42583.java
@@ -2,25 +2,40 @@ package queuestack;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class P42583 {
     @Test
     void name() {
-        System.out.println(Arrays.toString(sol(5, -5, 3, new int[]{-1, -3, 3})));
+        Solution s = new Solution();
+        int[] weights = new int[]{7, 4, 5, 6};
+        System.out.println(s.solution(2, 10, weights));
     }
 
-    public int[] sol(int n, int min, int max, int[] positions) {
-        int step = (max - min) / n;
-        Set<Integer> sets = Arrays.stream(positions).boxed().collect(Collectors.toSet());
-        int[] ints = IntStream.iterate(min, i -> i + step).limit(max + 1)
-                .filter(i -> !sets.contains(i))
-                .sorted()
-                .toArray();
-        return ints;
+    static class Solution {
+        public int solution(int bridge_length, int weight, int[] truck_weights) {
+            List<Integer> complete = new ArrayList<>();
+            Queue<Integer> bridge = new LinkedList<>();
+            int max = 0;
+            int answer = 0;
+            int index = 0;
+            while (complete.size() < truck_weights.length) {
+                answer++;
+                int truck = truck_weights[index];
+                if (!bridge.isEmpty() && complete.contains(answer)) {
+                    max -= bridge.poll();
+                }
+                if (max + truck <= weight) {
+                    bridge.add(truck);
+                    complete.add(answer + bridge_length);
+                    max += truck;
+                    index++;
+                }
+            }
+            return answer + bridge_length;
+        }
     }
-
 }
