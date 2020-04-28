2ed91b3b8ab6cbf1c891775280eb672ebb92bfa2
andole98 <andole87@gmail.com>
2020-04-28 16:07:28 +0900

    boj: 5339

---
+++ boj/java/B5339.java
@@ -0,0 +1,15 @@
+public class B5339 {
+    public static void main(String[] args) throws Exception {
+        String s = "     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |";
+        System.out.println(s);
+    }
+}
