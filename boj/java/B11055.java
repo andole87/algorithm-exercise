06ed523a0a711a1dc0c787e781d4dc48a2779403
andole98 <andole87@gmail.com>
2020-05-06 16:07:28 +0900

    boj: 11055

---
+++ boj/java/B11055.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B11055 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+    }
+}
