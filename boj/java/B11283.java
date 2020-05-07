2d41b816509bbf5aab54d3675500739d4bacc9a2
andole98 <andole87@gmail.com>
2020-05-07 16:07:28 +0900

    boj: 11283

---
+++ boj/java/B11283.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B11283 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+        System.out.println(((int) chars[0]) - 44031);
+
+    }
+}
