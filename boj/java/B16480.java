26e59d45907350d4af40def9e868909368c8192c
andole98 <andole87@gmail.com>
2020-07-09 15:53:49 +0900

    boj: 16480

---
+++ boj/java/B16480.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B16480 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long R = Integer.parseInt(st.nextToken());
+        long r = Integer.parseInt(st.nextToken());
+        System.out.println(R * (R - 2 * r));
+    }
+}
